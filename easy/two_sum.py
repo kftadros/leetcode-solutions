45eaae222d9711e1d9745e87f19192eec6938705
Kareem Tadros <kftadros@gmail.com>
2026-05-27 10:36:55 -0700

    Optimize Two Sum - hash map solution O(n)

--- easy/two_sum.py
+++ easy/two_sum.py
@@ -14,17 +14,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Check every combination of two numbers
+        seen = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                # Make sure we're not using the same element twice
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
-
-
-# Test cases
-if __name__ == "__main__":
-    sol = Solution()
-    print(sol.twoSum([2, 7, 11, 15], 9))  # [0, 1]
-    print(sol.twoSum([3, 2, 4], 6))       # [1, 2]
-    print(sol.twoSum([3, 3], 6))          # [0, 1]
+            needed = target - nums[i]
+            
+            if needed in seen:
+                return [seen[needed], i]
+            
+            seen[nums[i]] = i
